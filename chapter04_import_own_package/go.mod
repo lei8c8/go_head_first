92c02764bea1756c58abf72a1ae06a5ecadb2c03
tony <lei8c8@gmail.com>
2024-11-28 09:28:09 -0500

    finished learning chapter04

---
+++ chapter04_import_own_package/go.mod
@@ -0,0 +1,3 @@
+module go_head_first/chapter04_import_own_package
+
+go 1.23.3
---
+++ chapter04_import_own_package/main.go
@@ -0,0 +1,8 @@
+package main 
+
+import "go_head_first/chapter04_import_own_package/greeting"
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+}
